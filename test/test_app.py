b6df55c98e3e46bdb48a2b3a740bff107b3fb51e
Julien <jpa@frianbiz.com>
2022-10-11 00:56:56 +0200

    feat: test

--- test/test_app.py
+++ test/test_app.py
@@ -1,13 +1,36 @@
 import pytest
 
-from app import connexion
+from app import create_app
 
 
 @pytest.fixture
-def connexion(user):
-    rv = user.post(
-        "/connexion", data=dict(username='julien', password='password'), follow_redirects=True
+def client():
+    app = create_app({"TEST": True})
+    clear_all()
+    with app.test_client() as client:
+        yield client
+
+
+def _create_user(client, name, email, password):
+    rv = client.post(
+        "/register",
+        data=dict(name=name, email=email, password=password),
+        follow_redirects=True,
     )
     assert rv.status_code == 200
     data = rv.data.decode()
-    assert data.find("Login User") == -1
+    assert data.find("Inscrit") == -1
+
+
+def _login_user(client, email, password):
+    rv = client.post(
+        "/login", data=dict(email=email, password=password), follow_redirects=True
+    )
+    assert rv.status_code == 200
+    data = rv.data.decode()
+    assert data.find("Connecté") == -1
+
+
+def _logout_user(client):
+    rv = client.get("/logout", follow_redirects=True)
+    assert rv.status_code == 200
